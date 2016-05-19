dcbef35770fe1897f380ecc4e30faabdfa366a1d
Gleyder Hernandez <gleyderwebs@gmail.com>
2016-05-19 08:37:44 -1200

    Pagina de prueba

---
+++ Lalala/Lalala/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lalala.MvcApplication" Language="C#" %>
---
+++ Lalala/Lalala/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lalala.Startup))]
+namespace Lalala
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
